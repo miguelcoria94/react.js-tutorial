c693fa4f7908f8c4845016acb3d8dafbd00c0831
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-14 00:08:16 -0600

    create a board component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Board extends React.Component {
+  renderSqaure(i) {
+    return <Square value={i} />;
+  }
+}
+
+class Square
