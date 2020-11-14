5b0b8b145773c81c6889cf3263c1eea84d6ebc0c
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-14 00:54:28 -0600

    add x on click

--- src/index.js
+++ src/index.js
@@ -11,11 +11,11 @@ class Square extends React.Component {
         };
     }
 
-    
+
     render() {
         return (
-            <button className="square" onClick={() => {}}>
-                {this.props.value}
+            <button className="square" onClick={() => this.setState({value: 'X'})}>
+                {this.state.value}
             </button>
         );
     }
