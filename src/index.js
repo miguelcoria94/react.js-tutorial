6e9594da5b6e400328bba722d9eea1d2a4e10c37
miguelcoria94 <miguelcoria94@gmail.com>
2020-11-14 00:47:41 -0600

    create constructor for square

--- src/index.js
+++ src/index.js
@@ -4,9 +4,17 @@ import './index.css'
 
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
+    
     render() {
         return (
-            <button className="square">
+            <button className="square" onClick={() => {}}>
                 {this.props.value}
             </button>
         );
